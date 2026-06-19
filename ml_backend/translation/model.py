543187e02c1d9fecd7afc5d4ea46013174fa2143
Abiodun Longe <abiodun425longe@gmail.com>
2026-06-19 04:09:24 +0100

    fix(translation): replace HF inference API with local NLLB-200 model
    Added translation/.
    requirements.txt updated with pydantic, transformers, torch and sentencepiece

---
+++ ml_backend/translation/model.py
@@ -0,0 +1,33 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+MODEL_ID = "facebook/nllb-200-distilled-600M"
+SRC_LANG = "eng_Latn"
+TGT_LANG = "yor_Latn"
+MAX_NEW_TOKENS = 256
+
+
+class TranslationModel:
+    def __init__(self) -> None:
+        self._tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+        self._model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_ID)
+        self._model.eval()
+
+    def translate(self, text: str) -> str:
+        inputs = self._tokenizer(
+            text,
+            return_tensors="pt",
+            src_lang=SRC_LANG,
+        )
+
+        target_lang_id = self._tokenizer.convert_tokens_to_ids(TGT_LANG)
+
+        output_tokens = self._model.generate(
+            **inputs,
+            forced_bos_token_id=target_lang_id,
+            max_new_tokens=MAX_NEW_TOKENS,
+        )
+
+        return self._tokenizer.batch_decode(
+            output_tokens,
+            skip_special_tokens=True,
+        )[0]
